f3fa5018a5704a5723d483a99ad573901fb151c1
Donatas <doweris@gmail.com>
2021-05-12 00:05:18 +0300

    Added input change and regex for number testing

--- eshop/src/common/components/Counter.jsx
+++ eshop/src/common/components/Counter.jsx
@@ -1,15 +1,21 @@
 import { useState } from "react";
 
-
 function Counter(){
 
-    const [count, setCount] = useState(0);
+    const [count, setCount] = useState(1);
+
+    const onChange = (event) => {
+        var input = event.target.value;
+        var reg = /^-?[0-9]\d*(\.\d+)?$/;
+        if(!reg.test(input)) return;
+        setCount(parseInt(input));
+    };
 
     return (
-        <div>
-            <button className="font-bold text-lg hover:text-blue-600" onClick={() => setCount(count - 1)}>-</button>
-            <input className="w-10 text-center" value={count} />
-            <button className="font-bold text-lg hover:text-blue-600" onClick={() => setCount(count + 1)}>+</button>
+        <div className="p-5 flex justify-left">
+            <button className="font-thin text-xl hover:text-blue-600 border border-black px-3 bg-white" onClick={() => setCount(count - 1)}>-</button>
+            <input className="w-20 text-center border border-black" value={count} onChange={onChange} />
+            <button className="font-thin text-xl hover:text-blue-600 border border-black px-3 bg-white" onClick={() => setCount(count + 1)}>+</button>
         </div>
     )
 }
