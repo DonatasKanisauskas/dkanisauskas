0beaa45f697ccf693f8a4fafd3570152906acd0c
Donatas <doweris@gmail.com>
2021-05-11 19:16:40 +0300

    navbar styling

--- eshop/src/nav-bar/NavBar.jsx
+++ eshop/src/nav-bar/NavBar.jsx
@@ -2,11 +2,11 @@ import { Link } from "react-router-dom";
 
 function NavBar(){
     return <nav className="bg-gray-300">
-        <ul className="flex">
-            <li className="p-2">
+        <ul className="flex font-semibold text-lg">
+            <li className="p-2 hover:text-blue-600">
                 <Link to="/">PRODUCTS</Link>
             </li>
-            <li className="p-2">
+            <li className="p-2 hover:text-blue-600">
                 <Link to="/vision">VISION</Link>
             </li>
         </ul>
