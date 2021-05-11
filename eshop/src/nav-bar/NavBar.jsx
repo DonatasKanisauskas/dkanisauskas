ffb36ac0e0430f8e13bb93efaccba68e62d3fecf
Donatas <doweris@gmail.com>
2021-05-11 19:11:37 +0300

    small css changes

--- eshop/src/nav-bar/NavBar.jsx
+++ eshop/src/nav-bar/NavBar.jsx
@@ -1,5 +1,16 @@
+import { Link } from "react-router-dom";
+
 function NavBar(){
-    return <nav>Navbar</nav>
+    return <nav className="bg-gray-300">
+        <ul className="flex">
+            <li className="p-2">
+                <Link to="/">PRODUCTS</Link>
+            </li>
+            <li className="p-2">
+                <Link to="/vision">VISION</Link>
+            </li>
+        </ul>
+    </nav>
 }
 
 export default NavBar;
