6be36c74002f92ae02042e795ef9ccde544a9a04
Donatas <doweris@gmail.com>
2021-05-12 10:11:17 +0300

    changed input type to number, removed regex

--- eshop/src/common/components/Counter.jsx
+++ eshop/src/common/components/Counter.jsx
@@ -6,15 +6,15 @@ function Counter(){
 
     const onChange = (event) => {
         var input = event.target.value;
-        var reg = /^-?[0-9]\d*(\.\d+)?$/; 
-        if(!reg.test(input)) return;
+        // var reg = /^-?[0-9]\d*(\.\d+)?$/;
+        // if(!reg.test(input)) return;
         setCount(parseInt(input));
     };
 
     return (
         <div className="p-5 flex justify-left">
             <button className="font-thin text-xl hover:text-blue-600 border border-black px-3 bg-white" onClick={() => setCount(prev => prev - 1)}>-</button>
-            <input className="w-20 text-center border border-black" value={count} onChange={onChange} />
+            <input className="w-20 text-center border border-black" value={count} onChange={onChange} type="number" />
             <button className="font-thin text-xl hover:text-blue-600 border border-black px-3 bg-white" onClick={() => setCount(prev => prev + 1)}>+</button>
         </div>
     )
