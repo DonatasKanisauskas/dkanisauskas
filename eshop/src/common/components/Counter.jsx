a507d33f6b66889cd5910c2ed79f2d26c7ec5284
Donatas <doweris@gmail.com>
2021-05-12 10:17:16 +0300

    removed unnecessary comments and changed little bit styling

--- eshop/src/common/components/Counter.jsx
+++ eshop/src/common/components/Counter.jsx
@@ -5,17 +5,14 @@ function Counter(){
     const [count, setCount] = useState(1);
 
     const onChange = (event) => {
-        var input = event.target.value;
-        // var reg = /^-?[0-9]\d*(\.\d+)?$/;
-        // if(!reg.test(input)) return;
-        setCount(parseInt(input));
+        setCount(parseInt(event.target.value));
     };
 
     return (
         <div className="p-5 flex justify-left">
-            <button className="font-thin text-xl hover:text-blue-600 border border-black px-3 bg-white" onClick={() => setCount(prev => prev - 1)}>-</button>
-            <input className="w-20 text-center border border-black" value={count} onChange={onChange} type="number" />
-            <button className="font-thin text-xl hover:text-blue-600 border border-black px-3 bg-white" onClick={() => setCount(prev => prev + 1)}>+</button>
+            <button className="font-thin text-xl hover:bg-indigo-50 border border-black px-3 bg-white" onClick={() => setCount(prev => prev - 1)}>-</button>
+            <input className="w-20 text-center hover:bg-indigo-50 border border-black" value={count} onChange={onChange} type="number" />
+            <button className="font-thin text-xl hover:bg-indigo-50 border border-black px-3 bg-white" onClick={() => setCount(prev => prev + 1)}>+</button>
         </div>
     )
 }
