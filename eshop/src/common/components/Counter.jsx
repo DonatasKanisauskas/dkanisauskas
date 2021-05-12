0b4d49ed59b240d1f148630e1fc377ae2d45f490
Donatas <doweris@gmail.com>
2021-05-12 09:48:26 +0300

    Added previous number when using setCount

--- eshop/src/common/components/Counter.jsx
+++ eshop/src/common/components/Counter.jsx
@@ -6,16 +6,16 @@ function Counter(){
 
     const onChange = (event) => {
         var input = event.target.value;
-        var reg = /^-?[0-9]\d*(\.\d+)?$/;
+        var reg = /^-?[0-9]\d*(\.\d+)?$/; 
         if(!reg.test(input)) return;
         setCount(parseInt(input));
     };
 
     return (
         <div className="p-5 flex justify-left">
-            <button className="font-thin text-xl hover:text-blue-600 border border-black px-3 bg-white" onClick={() => setCount(count - 1)}>-</button>
+            <button className="font-thin text-xl hover:text-blue-600 border border-black px-3 bg-white" onClick={() => setCount(prev => prev - 1)}>-</button>
             <input className="w-20 text-center border border-black" value={count} onChange={onChange} />
-            <button className="font-thin text-xl hover:text-blue-600 border border-black px-3 bg-white" onClick={() => setCount(count + 1)}>+</button>
+            <button className="font-thin text-xl hover:text-blue-600 border border-black px-3 bg-white" onClick={() => setCount(prev => prev + 1)}>+</button>
         </div>
     )
 }
