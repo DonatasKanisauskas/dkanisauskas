20d95543da1d504f19e9e82e0c2d5ddb825d14b3
Donatas <doweris@gmail.com>
2021-05-17 00:10:57 +0300

    added key to NavItems, slightly edited come to improve readability, removed inout number scroller/arrows.

--- eshop/src/nav-bar/NavBar.jsx
+++ eshop/src/nav-bar/NavBar.jsx
@@ -1,18 +1,20 @@
 import NavItem from "./components/NavItem";
 
 const NavItems = [
-    {text: 'PRODUCTS', route: '/'},
-    {text: 'VISION', route: '/vision'},
+    {text: 'PRODUCTS', route: '/', key: 0},
+    {text: 'VISION', route: '/vision', key: 1},
 ];
 
 function NavBar(){
-    return <nav className="bg-gray-300">
-        <ul className="flex font-semibold text-lg">
-            {NavItems.map((item) => (
-                <NavItem {...item} />
-            ))}
-        </ul>
-    </nav>
+    return (
+        <nav className="bg-gray-300">
+            <ul className="flex font-semibold text-lg">
+                {NavItems.map((item) => (
+                    <NavItem {...item} />
+                ))}
+            </ul>
+        </nav>
+    )
 }
 
 export default NavBar;
--- eshop/src/nav-bar/components/NavItem.jsx
+++ eshop/src/nav-bar/components/NavItem.jsx
@@ -1,6 +1,6 @@
 import { Link } from "react-router-dom";
 
-function NavItem({ text, route }){
+function NavItem({ text, route}){
     return (
         <li className="p-2 hover:text-blue-600">
             <Link to={ route }>{ text }</Link>
