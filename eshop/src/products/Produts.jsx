2b89236cc8a6a6b7dac882298ec9cf74d92c5bec
Donatas <doweris@gmail.com>
2021-05-18 19:25:33 +0300

    useEffects, fetching git products

--- eshop/src/products/Produts.jsx
+++ eshop/src/products/Produts.jsx
@@ -1,10 +1,26 @@
-import Counter from "../common/components/Counter";
+import { useEffect, useState } from "react";
 
 function Products() {
+  const [products, setProducts] = useState([]);
+
+  useEffect(() => {
+    const getProducts = async () => {
+      const response = await fetch("https://fakestoreapi.com/products");
+      const products = await response.json();
+      setProducts(products);
+    };
+
+    getProducts();
+  }, []);
+
   return (
-    <div>
-      <Counter />
-    </div>
+    <ul>
+      {products.map((product) => (
+        <li className="p-3" key={product.id}>
+          {product.title}
+        </li>
+      ))}
+    </ul>
   );
 }
 
