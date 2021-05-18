7febaacf8d71cf4ea0ffa167710f287d61bc081b
Donatas <doweris@gmail.com>
2021-05-18 20:53:17 +0300

    aligning and styling cards with flex, reademe description update for lesson 4

--- README.md
+++ README.md
@@ -1,16 +1,30 @@
 # ASW Front end Academy (Donatas Kanišauskas)
 
 ## Lesson 1
+
 Readme.md update and pull request.
-___
+
+---
 
 ## Lesson 2
- - Created new React project.
- - Installed Croco and Tailwind.
-___
+
+- Created new React project.
+- Installed Croco and Tailwind.
+
+---
 
 ## Lesson 3
- - Installed react-router-dom.
- - Added Vision and Products components.
- - React hooks introduction (Added counter).
- - Counter buttons and input update using state hooks.
+
+- Installed react-router-dom.
+- Added Vision and Products components.
+- React hooks introduction (Added counter).
+- Counter buttons and input update using state hooks.
+
+---
+
+## Lesson 4
+
+- Added Fetch API with Async/await.
+- Fetched products from url to productList state.
+- Displayed products from product list in cards.
+- Cards styling with Tailwind.
--- eshop/src/products/Produts.jsx
+++ eshop/src/products/Produts.jsx
@@ -8,25 +8,35 @@ function Products() {
       const response = await fetch("https://fakestoreapi.com/products");
       const products = await response.json();
       setProducts(products);
+      console.log(products);
     };
 
     getProducts();
   }, []);
 
   return (
-    <ul className="flex flex-wrap">
+    <ul className="flex flex-wrap justify-around">
       {products.map((product) => (
         <li className="p-3" key={product.id}>
-          <div className="bg-white border w-64 h-80 hover:shadow-lg rounded">
-            <div className="h-3/4">
+          <div className="bg-white border w-64 h-90 hover:shadow-xl p-2">
+            <div className="h-48 flex items-stretch">
               <img
                 src={product.image}
                 alt={product.name}
-                className="max-h-full m-auto"
+                className="max-h-full m-auto self-center"
               />
             </div>
-            <div>{product.title}</div>
-            <div></div>
+            <div className="text-center font-semibold mt-3">
+              {product.price}$
+            </div>
+            <div className="truncate text-center p-2" title={product.title}>
+              {product.title}
+            </div>
+            <div className="grid justify-items-stretch">
+              <button className="justify-self-center bg-gray-500 hover:bg-gray-600 w-32 h-10  text-white rounded">
+                VIEW
+              </button>
+            </div>
           </div>
         </li>
       ))}
