ab1117bc34aa4def3f11d223a2eef9ebadd4ad2a
Donatas <doweris@gmail.com>
2021-06-09 14:07:02 +0300

    moved displayDots function under useEffect

--- eshop/src/common/components/Spinner.jsx
+++ eshop/src/common/components/Spinner.jsx
@@ -4,14 +4,14 @@ import { ReactComponent as SpinIcon } from "../assets/spin.svg";
 function Spinner({ text = "Loading" }) {
   const [dots, setDots] = useState("");
 
-  const displayDots = () => {
-    setDots((prev) => (prev.length >= 3 ? "" : `${prev}.`));
-  };
-
   useEffect(() => {
     const interval = setInterval(displayDots, 600);
     return () => clearInterval(interval);
   }, []);
+
+  const displayDots = () => {
+    setDots((prev) => (prev.length >= 3 ? "" : `${prev}.`));
+  };
 
   return (
     <div className="h-full flex flex-col justify-center items-center">
